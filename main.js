0c776d527217f7f0839950ea5e19ee0364bf98e3
yeyoung8805 <yyk8805@naver.com>
2022-08-31 17:42:39 +0900

    can send str parameter as event

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 function a(callback) {
+  const str = "Hello A";
   setTimeout(() => {
     console.log("A");
-    callback();
+    callback(str);
   }, 1000);
 }
 
 function b() {
   console.log("B");
 }
-a(function () {
+a(function (event) {
+  console.log(event);
   b();
+});
+
+//for information
+el.addEventListener("click", function (event) {
+  console.log(event);
 });
