32e9628dacbe24f22e4c0c68f0d7b3a2d356b03a
yeyoung8805 <yyk8805@naver.com>
2022-08-31 17:54:50 +0900

    example of too much callbacks

--- main.js
+++ main.js
@@ -12,8 +12,26 @@ function b(callback) {
   }, 1000);
 }
 
+function c(callback) {
+  setTimeout(() => {
+    console.log("C");
+    callback();
+  }, 1000);
+}
+
+function d(callback) {
+  setTimeout(() => {
+    console.log("D");
+    callback();
+  }, 1000);
+}
+
 a(function () {
   b(function () {
-    console.log("Done!");
+    c(function () {
+      d(function () {
+        console.log("Done!");
+      });
+    });
   });
 });
