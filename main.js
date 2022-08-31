901ad1e79e90b5f5d760845cd8b3bcc8bad82b29
yeyoung8805 <yyk8805@naver.com>
2022-08-31 18:01:03 +0900

    run functions in orderly

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
-function a(callback) {
-  setTimeout(() => {
-    console.log("A");
-    callback();
-  }, 1000);
-}
+// function a(callback) {
+//   setTimeout(() => {
+//     console.log("A");
+//     callback();
+//   }, 1000);
+// }
 
-function b(callback) {
-  setTimeout(() => {
-    console.log("B");
-    callback();
-  }, 1000);
-}
-
-function c(callback) {
-  setTimeout(() => {
-    console.log("C");
-    callback();
-  }, 1000);
-}
-
-function d(callback) {
-  setTimeout(() => {
-    console.log("D");
-    callback();
-  }, 1000);
-}
-
-a(function () {
-  b(function () {
-    c(function () {
-      d(function () {
-        console.log("Done!");
-      });
-    });
+function a() {
+  //promise: 약속의 객체!
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log("A");
+      resolve();
+    }, 1000);
   });
-});
+}
+
+function b() {
+  console.log("B");
+}
+async function test() {
+  await a();
+  b();
+}
+test();
