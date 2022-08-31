ec05474d0393cd1ccc1e5b414f609927f9dc2742
yeyoung8805 <yyk8805@naver.com>
2022-08-31 17:22:14 +0900

    to make b() called later than a(), use callback()

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-function a() {
+function a(callback) {
   setTimeout(() => {
     console.log("A");
+    callback();
   }, 1000);
 }
 
 function b() {
   console.log("B");
 }
-a();
-b();
+a(function () {
+  b();
+});
